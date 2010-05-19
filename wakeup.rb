449b3f0f7c9e3433867fbe80c333a9ebd35cf06f
Larry Karnowski <larry.karnowski@gmail.com>
2010-05-18 22:48:01 -0400

    Finally got it to add an mp3 to iTunes!

--- wakeup.rb
+++ wakeup.rb
@@ -17,12 +17,29 @@ end
 # playlist.playOnce(false) if playlist
 
 playlist = itunes.sources["Library"].userPlaylists["Listen At Least Once"]
-playlist.fileTracks.each do |track|
+# playlist.fileTracks.each do |track|
+# 
+#   if track.location
+#     puts "#{track.artist} - #{track.album} - #{track.name}"
+#     puts track.location.path
+#   # else
+#   #   puts "WARNING: iTunes cannot find the song file for: #{track.artist} - #{track.name}"
+#   end
+# end
 
-  if track.location
-    puts "#{track.artist} - #{track.album} - #{track.name}"
-    puts track.location.path
-  # else
-  #   puts "WARNING: iTunes cannot find the song file for: #{track.artist} - #{track.name}"
-  end
-end
+
+song = "/Users/larry/Desktop/Whispering Pines/Family Tree/01 Family Tree.mp3"
+# song = ":Users:larry:Desktop:Whispering Pines:Family Tree:01 Family Tree.mp3"
+# file_track = ITunesFileTrack.alloc.init
+# puts file_track.inspect
+# file_track.setLocation(song)
+url = NSURL.alloc.initFileURLWithPath(song)
+puts url.absoluteString.inspect
+# file_track.location = url
+# puts file_track.location.inspect
+array = NSArray.alloc.initWithArray([url])
+#array = NSArray.new([song_file])
+track = itunes.add(array, :to => playlist)
+puts track.inspect
+
+
