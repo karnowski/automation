c8cf3582b32d8db8ca2f4eba51c05e6c9fa0ff1f
Larry Karnowski <larry.karnowski@gmail.com>
2010-05-18 22:50:42 -0400

    Adding more than one file at a time.

--- wakeup.rb
+++ wakeup.rb
@@ -11,35 +11,12 @@ class SBElementArray
     self.objectWithName(value)
   end
 end
- 
-#itunes.stop
-# playlist = itunes.sources["Library"].userPlaylists["Top 100 Most Played"]
-# playlist.playOnce(false) if playlist
 
 playlist = itunes.sources["Library"].userPlaylists["Listen At Least Once"]
-# playlist.fileTracks.each do |track|
-# 
-#   if track.location
-#     puts "#{track.artist} - #{track.album} - #{track.name}"
-#     puts track.location.path
-#   # else
-#   #   puts "WARNING: iTunes cannot find the song file for: #{track.artist} - #{track.name}"
-#   end
-# end
-
-
-song = "/Users/larry/Desktop/Whispering Pines/Family Tree/01 Family Tree.mp3"
-# song = ":Users:larry:Desktop:Whispering Pines:Family Tree:01 Family Tree.mp3"
-# file_track = ITunesFileTrack.alloc.init
-# puts file_track.inspect
-# file_track.setLocation(song)
-url = NSURL.alloc.initFileURLWithPath(song)
-puts url.absoluteString.inspect
-# file_track.location = url
-# puts file_track.location.inspect
-array = NSArray.alloc.initWithArray([url])
-#array = NSArray.new([song_file])
+song1 = "/Users/larry/Desktop/Whispering Pines/Family Tree/01 Family Tree.mp3"
+song2 = "/Users/larry/Desktop/Whispering Pines/Family Tree/02 Brand New Beat.mp3"
+url1 = NSURL.alloc.initFileURLWithPath(song1)
+url2 = NSURL.alloc.initFileURLWithPath(song2)
+array = NSArray.alloc.initWithArray([url1, url2])
 track = itunes.add(array, :to => playlist)
 puts track.inspect
-
-
